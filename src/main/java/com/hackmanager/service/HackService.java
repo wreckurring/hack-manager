a8fd1ae317b5ad4b5c1649773582f2ee514756cf
wreckurring <wreckurring@gmail.com>
2026-03-23 23:46:14 +0530

    .

--- src/main/java/com/hackmanager/service/HackService.java
+++ src/main/java/com/hackmanager/service/HackService.java
@@ -8,6 +8,63 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @Service
 public class HackService {
 
